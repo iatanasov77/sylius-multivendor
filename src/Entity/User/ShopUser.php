91f4a06f2aba03f39eae1d751faf8fd36df945b5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-03-16 18:44:05 +0000

    Update ShopUser Entity.

--- src/Entity/User/ShopUser.php
+++ src/Entity/User/ShopUser.php
@@ -7,12 +7,16 @@ namespace App\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\ShopUser as BaseShopUser;
 
+use Vankosoft\SyliusMultiVendor\Model\Interfaces\MultiVendorShopUserInterface;
+use Vankosoft\SyliusMultiVendor\Model\Traits\MultiVendorShopUserEntity;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_shop_user")
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_shop_user')]
-class ShopUser extends BaseShopUser
+class ShopUser extends BaseShopUser implements MultiVendorShopUserInterface
 {
+    use MultiVendorShopUserEntity;
 }
