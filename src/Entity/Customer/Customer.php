b5ea3951846d997cd3a6f1ef17b38589b4a31c79
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-03-16 20:52:43 +0000

    Move Vendor From  ShopUser to Customer .

--- src/Entity/Customer/Customer.php
+++ src/Entity/Customer/Customer.php
@@ -7,12 +7,16 @@ namespace App\Entity\Customer;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Customer as BaseCustomer;
 
+use Vankosoft\SyliusMultiVendor\Model\Interfaces\MultiVendorShopUserInterface;
+use Vankosoft\SyliusMultiVendor\Model\Traits\MultiVendorShopUserEntity;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_customer")
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_customer')]
-class Customer extends BaseCustomer
+class Customer extends BaseCustomer implements MultiVendorShopUserInterface
 {
+    use MultiVendorShopUserEntity;
 }
